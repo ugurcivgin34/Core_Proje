d636fbd66d9fbbd9197b597b930a0a25fb95938b
okan çivgin <okanbjkspd@hotmail.com>
2022-09-14 22:09:44 +0300

    Duyurular sayfası tamamlandı,dashboad sayfası düzenlendi , webden api ile veri alındı

---
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -0,0 +1,60 @@
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class ProfileController : Controller
+    {
+
+        private readonly UserManager<WriterUser> _userManager;
+
+        public ProfileController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            UserEditViewModel model = new UserEditViewModel();
+            model.Name = values.Name;
+            model.Surname = values.Surname;
+            model.PictureURL = values.ImageUrl;
+            return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+            if (p.Picture != null)
+            {
+                var resource = Directory.GetCurrentDirectory(); //yolu al
+                var extension = Path.GetExtension(p.Picture.FileName);//parametreden gelen picture in pathnı alacak
+                var imagename = Guid.NewGuid() + extension; //BEnzersiz resim ismi oluşturdkk
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;//resmin kaydedilecei lokasyon
+                var stream = new FileStream(savelocation, FileMode.Create);//resim dosyası oluştuldu
+                await p.Picture.CopyToAsync(stream);
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            user.PasswordHash = _userManager.PasswordHasher.HashPassword(user, p.Password);
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            return View();
+        }
+    }
+}
