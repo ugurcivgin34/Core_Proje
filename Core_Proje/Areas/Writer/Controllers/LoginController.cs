4635719563d9a6b2d01166a826d23126b7733c58
okan çivgin <okanbjkspd@hotmail.com>
2022-09-27 08:37:50 +0300

    Admin Alanı Bazı Detaylar Gerçekleştirildi,Sosyal Medya ve Referanslar Kısmı Tamamlandı,Admin Üst Menü tarafı tamamlandı,Rolleme işlemi gerçekleştirildi,Ajax kullanılarak deneyim kısmında crud işlemleri gerçekleştirildi

--- Core_Proje/Areas/Writer/Controllers/LoginController.cs
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -1,5 +1,6 @@
 ﻿using Core_Proje.Areas.Writer.Models;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -9,7 +10,9 @@ using System.Threading.Tasks;
 
 namespace Core_Proje.Areas.Writer.Controllers
 {
+    [AllowAnonymous]
     [Area("Writer")]
+    [Route("Writer/[controller]/[action]")]
     public class LoginController : Controller
     {
         private readonly SignInManager<WriterUser> _signInManager;
